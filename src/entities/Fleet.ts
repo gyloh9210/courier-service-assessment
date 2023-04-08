b1fe40c71dffe64702a98dd57a5ef3d7135a2750
gyloh <ronaldo_loh@hotmail.com>
2023-04-08 16:56:39 +0800

    add fleet along test cases

---
+++ src/entities/Fleet.ts
@@ -0,0 +1,223 @@
+import Parcel from './Parcel';
+
+type Courier = {
+  totalTimeTaken: number;
+  id: number;
+};
+
+class Fleet {
+  parcels: Parcel[];
+  maxSpeed: number;
+  maxLoad: number;
+  vehicleQty: number;
+
+  constructor({
+    parcels,
+    maxSpeed,
+    maxLoad,
+    vehicleQty
+  }: {
+    parcels: Parcel[];
+    maxSpeed: number;
+    maxLoad: number;
+    vehicleQty: number;
+  }) {
+    this.parcels = parcels;
+    this.maxSpeed = maxSpeed;
+    this.maxLoad = maxLoad;
+    this.vehicleQty = vehicleQty;
+  }
+
+  // Find subsets that add up to the target sum and return an array of parcel sets
+  findPossibleParcelSets(parcels: Parcel[], maxLoad: number): Parcel[][] {
+    // Extract the weight attribute from each Parcel object and create a list of weights
+    const weights: number[] = parcels.map((parcel) => parcel.weight);
+
+    // Sort the array in descending order
+    weights.sort((a, b) => b - a);
+
+    // Initialize an empty array to store the subsets
+    let subsets: Parcel[][] = [];
+
+    // Iterate over the weights and find subsets that add up to the target sum
+    for (let i = 0; i < weights.length; i++) {
+      const newSubsets: Parcel[][] = [];
+      const weight = weights[i];
+
+      for (let j = 0; j < subsets.length; j++) {
+        const subset = subsets[j];
+        let sum = 0;
+
+        for (let k = 0; k < subset.length; k++) {
+          const parcel = parcels.find(
+            (parcel) => parcel.id === subset[k].id
+          ) as Parcel;
+          sum += parcel.weight;
+        }
+
+        if (sum + weight <= maxLoad) {
+          newSubsets.push(
+            subset.concat(
+              parcels.find((parcel) => parcel.weight === weight) as Parcel
+            )
+          );
+        }
+      }
+
+      // Add the new subsets to the existing list of subsets
+      subsets = subsets.concat(newSubsets);
+
+      // Add the current weight as an individual subset if it does not exceed the target sum
+      if (weight <= maxLoad) {
+        subsets.push([
+          parcels.find((parcel) => parcel.weight === weight) as Parcel
+        ]);
+      }
+    }
+
+    // Filter out subsets that exceed the target sum and group the remaining subsets by Parcel objects
+    const groupedSubsets: Parcel[][] = [];
+
+    for (let i = 0; i < subsets.length; i++) {
+      const subset = subsets[i];
+      let sum = 0;
+
+      for (let j = 0; j < subset.length; j++) {
+        const parcel = parcels.find(
+          (parcel) => parcel.id === subset[j].id
+        ) as Parcel;
+        sum += parcel.weight;
+      }
+
+      if (sum <= maxLoad) {
+        groupedSubsets.push(subset);
+      }
+    }
+
+    return groupedSubsets;
+  }
+
+  // Sort parcel sets by heaviest weight and shortest distance
+  sortByPriority(parcelSets: Parcel[][]) {
+    return parcelSets.sort((a, b) => {
+      const aTotalWeight = a.reduce((acc, obj) => acc + obj.weight, 0);
+      const bTotalWeight = b.reduce((acc, obj) => acc + obj.weight, 0);
+      const aTotalDistance = a.reduce((acc, obj) => acc + obj.distance, 0);
+      const bTotalDistance = b.reduce((acc, obj) => acc + obj.distance, 0);
+
+      // If both weight are similar, take the shorter distance one
+      if (aTotalWeight === bTotalWeight) {
+        return aTotalDistance < bTotalDistance ? -1 : 1;
+      } else {
+        // Else, take the heaviest
+        return aTotalWeight > bTotalWeight ? -1 : 1;
+      }
+    });
+  }
+
+  // Deduplicate assigned parcels from a list
+  deduplicateParcelSets(parcelSets: Parcel[][], availableParcels: Parcel[]) {
+    let parcels = availableParcels;
+
+    return parcelSets.filter((parcelSet) => {
+      const originalParcelLength = parcels.length;
+
+      // Return parcels which does not being included in a set of assigned parcels.
+      // Eg: parcels = [a,b,c], parcelSet = [a,b], result = [c]
+      const filterParcels = parcels.filter(
+        (parcel) => !parcelSet.find((ps) => ps.id === parcel.id)
+      );
+
+      const filterParcelLength = filterParcels.length;
+
+      // if parcels have been found in a parcel set, carry the remain forward
+      if (originalParcelLength - filterParcelLength == parcelSet.length) {
+        parcels = filterParcels;
+        return true;
+      }
+
+      return false;
+    });
+  }
+
+  // Calculate delivery time for each parcel
+  calculateDeliveryTime(
+    parcelSets: Parcel[][],
+    vehicleQty: number,
+    maxSpeed: number
+  ) {
+    // Initialize a number of courier with 0 totalTimeTaken
+    let couriers: Courier[] = Array(vehicleQty)
+      .fill(undefined)
+      .map((c, index) => ({ id: index, totalTimeTaken: 0 }));
+
+    for (let i = 0; i < parcelSets.length; i++) {
+      // Take the smallest time as it will be available first
+      const availableCourier = couriers.sort(
+        (a, b) => a.totalTimeTaken - b.totalTimeTaken
+      )[0];
+
+      // Use it to add with next trip
+      const lastTripTimeTaken = availableCourier.totalTimeTaken;
+
+      for (let j = 0; j < parcelSets[i].length; j++) {
+        const parcel = parcelSets[i][j];
+        const deliveryTime =
+          Math.floor((parcel.distance / maxSpeed) * 100) / 100;
+
+        parcel.deliveryTime = (deliveryTime + lastTripTimeTaken).toFixed(2);
+
+        // IMPORTANT NOTE: Take the longest time as a time for an entire route
+        if (deliveryTime > availableCourier.totalTimeTaken) {
+          availableCourier.totalTimeTaken = deliveryTime;
+        }
+
+        parcelSets[i][j] = parcel;
+      }
+
+      // Total time for return trip
+      availableCourier.totalTimeTaken *= 2;
+
+      // Update back a courier
+      couriers = couriers.map((courier) => {
+        if (courier.id == availableCourier.id) {
+          return availableCourier;
+        }
+
+        return courier;
+      });
+    }
+
+    // Flat parcel[][] to parcel[] and sort by id in asc
+    return parcelSets.flat().sort((a, b) => {
+      if (a.id < b.id) {
+        return -1;
+      }
+      if (a.id > b.id) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  start(): Parcel[] {
+    const parcelSets = this.findPossibleParcelSets(this.parcels, this.maxLoad);
+
+    const parcelSetsByHeaviest = this.sortByPriority(parcelSets);
+
+    const finalizedParcelSets = this.deduplicateParcelSets(
+      parcelSetsByHeaviest,
+      this.parcels
+    );
+
+    const parcelsWithDeliveryTime = this.calculateDeliveryTime(
+      finalizedParcelSets,
+      this.vehicleQty,
+      this.maxSpeed
+    );
+
+    return parcelsWithDeliveryTime;
+  }
+}
+
+export default Fleet;
---
+++ test/entities/Fleet.spec.ts
@@ -0,0 +1,406 @@
+import Fleet from '../../src/entities/Fleet';
+import Parcel from '../../src/entities/Parcel';
+
+describe('Fleet', () => {
+  let fleet: Fleet;
+
+  beforeAll(() => {
+    fleet = new Fleet({
+      parcels: [],
+      maxSpeed: 70,
+      maxLoad: 200,
+      vehicleQty: 2
+    });
+  });
+
+  describe('#findPossibleParcelSets', () => {
+    it('should return a list of parcel sets which sum under a max load', () => {
+      const parcels = [
+        new Parcel({
+          id: 'pkg1',
+          weight: 50,
+          distance: 30,
+          baseDeliveryCost: 100
+        }),
+        new Parcel({
+          id: 'pkg2',
+          weight: 75,
+          distance: 125,
+          baseDeliveryCost: 100
+        }),
+        new Parcel({
+          id: 'pkg3',
+          weight: 175,
+          distance: 100,
+          baseDeliveryCost: 100
+        })
+      ];
+
+      const result = fleet.findPossibleParcelSets(parcels, fleet.maxLoad);
+
+      expect(result).toEqual([
+        [
+          new Parcel({
+            id: 'pkg3',
+            weight: 175,
+            baseDeliveryCost: 100,
+            distance: 100,
+            offer: null,
+            deliveryTime: ''
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg2',
+            weight: 75,
+            baseDeliveryCost: 100,
+            distance: 125,
+            offer: null,
+            deliveryTime: ''
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg2',
+            weight: 75,
+            baseDeliveryCost: 100,
+            distance: 125,
+            offer: null,
+            deliveryTime: ''
+          }),
+          new Parcel({
+            id: 'pkg1',
+            weight: 50,
+            baseDeliveryCost: 100,
+            distance: 30,
+            offer: null,
+            deliveryTime: ''
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg1',
+            weight: 50,
+            baseDeliveryCost: 100,
+            distance: 30,
+            offer: null,
+            deliveryTime: ''
+          })
+        ]
+      ]);
+    });
+  });
+
+  describe('#sortByPriority', () => {
+    it('should return parcels in heaviest and with shortest distance', () => {
+      const result = fleet.sortByPriority([
+        [
+          new Parcel({
+            id: 'pkg3',
+            weight: 100,
+            baseDeliveryCost: 100,
+            distance: 70
+          }),
+          new Parcel({
+            id: 'pkg2',
+            weight: 55,
+            baseDeliveryCost: 100,
+            distance: 30
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg5',
+            weight: 155,
+            baseDeliveryCost: 100,
+            distance: 95
+          })
+        ]
+      ]);
+
+      expect(result).toEqual([
+        [
+          new Parcel({
+            id: 'pkg5',
+            weight: 155,
+            baseDeliveryCost: 100,
+            distance: 95
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg3',
+            weight: 100,
+            baseDeliveryCost: 100,
+            distance: 70
+          }),
+          new Parcel({
+            id: 'pkg2',
+            weight: 55,
+            baseDeliveryCost: 100,
+            distance: 30
+          })
+        ]
+      ]);
+    });
+  });
+
+  describe('#deduplicateParcelSets', () => {
+    it('should return parcels which dont get repeated in other set', () => {
+      const parcels = [
+        new Parcel({
+          id: 'pkg1',
+          weight: 30,
+          baseDeliveryCost: 100,
+          distance: 20
+        }),
+        new Parcel({
+          id: 'pkg2',
+          weight: 55,
+          baseDeliveryCost: 100,
+          distance: 30
+        }),
+        new Parcel({
+          id: 'pkg3',
+          weight: 100,
+          baseDeliveryCost: 100,
+          distance: 70
+        }),
+        new Parcel({
+          id: 'pkg5',
+          weight: 155,
+          baseDeliveryCost: 100,
+          distance: 95
+        })
+      ];
+
+      const parcelSets = [
+        [
+          new Parcel({
+            id: 'pkg3',
+            weight: 100,
+            baseDeliveryCost: 100,
+            distance: 70
+          }),
+          new Parcel({
+            id: 'pkg2',
+            weight: 55,
+            baseDeliveryCost: 100,
+            distance: 30
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg5',
+            weight: 155,
+            baseDeliveryCost: 100,
+            distance: 95
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg1',
+            weight: 155,
+            baseDeliveryCost: 100,
+            distance: 95
+          }),
+          new Parcel({
+            id: 'pkg2',
+            weight: 55,
+            baseDeliveryCost: 100,
+            distance: 30
+          })
+        ]
+      ];
+
+      const result = fleet.deduplicateParcelSets(parcelSets, parcels);
+
+      expect(result).toEqual([
+        [
+          new Parcel({
+            id: 'pkg3',
+            weight: 100,
+            baseDeliveryCost: 100,
+            distance: 70
+          }),
+          new Parcel({
+            id: 'pkg2',
+            weight: 55,
+            baseDeliveryCost: 100,
+            distance: 30
+          })
+        ],
+        [
+          new Parcel({
+            id: 'pkg5',
+            weight: 155,
+            baseDeliveryCost: 100,
+            distance: 95
+          })
+        ]
+      ]);
+    });
+  });
+
+  describe('calculateDeliveryTime', () => {
+    it('should return a delivery time for each parcel correctly', () => {
+      const result = fleet.calculateDeliveryTime(
+        [
+          [
+            new Parcel({
+              id: 'pkg3',
+              weight: 100,
+              baseDeliveryCost: 100,
+              distance: 70
+            }),
+            new Parcel({
+              id: 'pkg2',
+              weight: 55,
+              baseDeliveryCost: 100,
+              distance: 30
+            })
+          ],
+          [
+            new Parcel({
+              id: 'pkg5',
+              weight: 155,
+              baseDeliveryCost: 100,
+              distance: 95
+            })
+          ],
+          [
+            new Parcel({
+              id: 'pkg1',
+              weight: 30,
+              baseDeliveryCost: 100,
+              distance: 20
+            })
+          ]
+        ],
+        fleet.vehicleQty,
+        fleet.maxSpeed
+      );
+
+      expect(result).toEqual([
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '2.28',
+          distance: 20,
+          id: 'pkg1',
+          offer: null,
+          weight: 30
+        }),
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '0.42',
+          distance: 30,
+          id: 'pkg2',
+          offer: null,
+          weight: 55
+        }),
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '1.00',
+          distance: 70,
+          id: 'pkg3',
+          offer: null,
+          weight: 100
+        }),
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '1.35',
+          distance: 95,
+          id: 'pkg5',
+          offer: null,
+          weight: 155
+        })
+      ]);
+    });
+  });
+
+  describe('#start', () => {
+    it('should calculate an estimated delivery time for each parcel', () => {
+      fleet.parcels = [
+        new Parcel({
+          id: 'pkg1',
+          weight: 50,
+          distance: 30,
+          baseDeliveryCost: 100
+        }),
+        new Parcel({
+          id: 'pkg2',
+          weight: 75,
+          distance: 125,
+          baseDeliveryCost: 100
+        }),
+        new Parcel({
+          id: 'pkg3',
+          weight: 175,
+          distance: 100,
+          baseDeliveryCost: 100
+        }),
+        new Parcel({
+          id: 'pkg4',
+          weight: 110,
+          distance: 60,
+          baseDeliveryCost: 100
+        }),
+        new Parcel({
+          id: 'pkg5',
+          weight: 155,
+          distance: 95,
+          baseDeliveryCost: 100
+        })
+      ];
+
+      const result = fleet.start();
+
+      expect(result).toEqual([
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '3.98',
+          distance: 30,
+          id: 'pkg1',
+          offer: null,
+          weight: 50
+        }),
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '1.78',
+          distance: 125,
+          id: 'pkg2',
+          offer: null,
+          weight: 75
+        }),
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '1.42',
+          distance: 100,
+          id: 'pkg3',
+          offer: null,
+          weight: 175
+        }),
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '0.85',
+          distance: 60,
+          id: 'pkg4',
+          offer: null,
+          weight: 110
+        }),
+        new Parcel({
+          baseDeliveryCost: 100,
+          deliveryTime: '4.19',
+          distance: 95,
+          id: 'pkg5',
+          offer: null,
+          weight: 155
+        })
+      ]);
+    });
+
+    afterAll(() => {
+      fleet.parcels = [];
+    });
+  });
+});
